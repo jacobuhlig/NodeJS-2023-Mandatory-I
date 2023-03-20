3a2b4cb031b016b4541bed81272597adb9cba1fa
Jacob Uhlig <jacobuhlig@gmail.com>
2023-03-20 19:47:38 +0100

    Finshed most of the actual design process.

--- util/templateEngine.js
+++ util/templateEngine.js
@@ -3,14 +3,16 @@ import escape from "escape-html";
 
 function renderPage(page, config={}) {
 
-  const navbar = fs.readFileSync("./public/components/navbar/navbar.html").toString()
+  const navbar = fs.readFileSync("./public/components/00._navbar/navbar.html").toString()
     .replace("$TAB_TITLE", config.tabTitle || "Mandatory I")
     .replace("$CSS_LINK", config.cssLink || "");
+
+  const sidebar = fs.readFileSync("./public/components/01._sidebar/sidebar.html").toString();
   
-    const footer = fs.readFileSync("./public/components/footer/footer.html").toString()
+  const footer = fs.readFileSync("./public/components/02._footer/footer.html").toString()
     .replace("$FOOTER_YEAR", `© ${new Date().getFullYear()}`);
 
-  return navbar + page + footer;
+  return navbar + sidebar + page + footer;
 }
 
 function readPage(pagePath) {
